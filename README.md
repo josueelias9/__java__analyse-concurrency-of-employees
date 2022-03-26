e0c337bc4c02de815a3d6607c74804ab79d5224a
Josué <josueeliashuaman@gmail.com>
2022-03-25 20:20:35 -0500

    Fist commit
    - Project created.
    - An introduction to the project.
    - Deploy of Spring framework.
    - json, jsoup dependancies installed.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# proyecto
+para IPTP: web scrapper
+
+# estructura
+```
+|-- frontend (frontend :D)
+|-- README.md (documentacion)
+|-- data_java-2.xml (XML de la empresa)
+|-- payroll (backend - spring bot)
+    |-- pom.xml (dependencias del proyecto)
+```
 # java
 top programming language 2022 [link](https://statisticsanddata.org/data/the-most-popular-programming-languages-1965-2022-new-update/#:~:text=As%20of%201%20January%202022,from%20third%20position%20to%20first.)
 
@@ -7,7 +18,14 @@ top programming language 2022 [link](https://statisticsanddata.org/data/the-most
 
 # teoria
 - final vs abstract [link](https://www.google.com/search?q=final+abstract+java&tbm=isch&ved=2ahUKEwj2yd_Kxd_2AhW6LLkGHbmKBYMQ2-cCegQIABAA&oq=final+abstract+java&gs_lcp=CgNpbWcQAzoHCCMQ7wMQJ1CEBli2DWCaD2gCcAB4AIABugGIAeAEkgEDMC40mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=uc48YraJBrrZ5OUPuZWWmAg&bih=575&biw=1294#imgrc=vcpN2vkTsVy5XM)
-- private vs default vs protected vs public [linkk](https://i.stack.imgur.com/JqGNs.png)
+- private vs default vs protected vs public [link](https://i.stack.imgur.com/JqGNs.png)
 - entity [link](https://docs.oracle.com/javaee/6/tutorial/doc/bnbqa.html)
 - arquitectura de spring boot (no oficial) [link](https://www.researchgate.net/figure/Fig-2-Architecture-flow-of-spring-boot-Applications-Spring-boot-uses-all-the-features_fig2_341151097)
 - arquitectura spring + react [link](https://www.youtube.com/watch?v=LCT4LPm5dnI)
+
+# deploy 
+- descargar repo git hub
+```bash
+cd (ubicacion)/java/payroll
+./mvnw spring-boot:run
+```
--- payroll/src/main/java/payroll/PayrollApplication.java
+++ payroll/src/main/java/payroll/PayrollApplication.java
@@ -2,11 +2,45 @@ package payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.parser.XmlTreeBuilder;
+import org.json.*;
 
 @SpringBootApplication
 public class PayrollApplication {
 
   public static void main(String... args) {
+    String xml= "<response>    <bar addedby='' comment='' datacenter='15' dateoffset='1' dcname='Gamma' enddate='2015-01-20 17:00:00' group='2' id='72780' isdbar='false' name='LoLa' startdate='2015-01-20 08:00:00' type='0' useroffset='2' usertype='0'/>    <bar addedby='' comment='' datacenter='15' dateoffset='1' dcname='Gamma' enddate='2015-01-23 17:00:00' group='2' id='72783' isdbar='false' name='LoLa' startdate='2015-01-23 08:00:00' type='0' useroffset='2' usertype='0'/>    <bar addedby='' comment='' datacenter='15' dateoffset='1' dcname='Gamma' enddate='2015-01-22 17:00:00' group='2' id='72782' isdbar='false' name='LoLa' startdate='2015-01-22 08:00:00' type='0' useroffset='2' usertype='0'/>    <bar addedby='' comment='' datacenter='15' dateoffset='1' dcname='Gamma' enddate='2015-01-27 17:00:00' group='2' id='72785' isdbar='false' name='LoLa' startdate='2015-01-27 08:00:00' type='0' useroffset='2' usertype='0'/>    <bar addedby='' comment='' datacenter='15' dateoffset='1' dcname='Gamma' enddate='2015-01-26 17:00:00' group='2' id='72784' isdbar='false' name='LoLa' startdate='2015-01-26 08:00:00' type='0' useroffset='2' usertype='0'/></response>  ";  
+    JSONObject json = XML.toJSONObject(xml); 
+    String jsonString = json.toString(4);  
+    //JSONParser parser = new JSONParser();
+    JSONObject a=(JSONObject) json.get("response");
+
+    //System.out.println(a);  
+    System.out.println(a.get("bar"));
+    //String html="<bar addedby='' comment='' datacenter='13' dateoffset='8' dcname='Alfa' enddate='2015-01-27 22:00:00' group='2' id='72986' isdbar='false' name='Jonh Smit' startdate='2015-01-27 09:00:00' type='0' useroffset='2' usertype='0'/>";
+    //Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+    //String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+    //Document doc = Jsoup.parse(html);
+    //Element link = doc.select("a").first();
+ //   for(Element e: doc.select("bar")){
+ //     System.out.println(e);
+ // }
+    //String linkHref = link.attr("href"); // "http://example.com/"
+    //String linkText = link.text(); // "example""
+    //
+    //String linkOuterH = link.outerHtml();
+    //// "<a href="http://example.com"><b>example</b></a>"
+    //String linkInnerH = link.html(); // "<b>example</b>"
+   // System.out.println(doc);
+    //System.out.println(linkHref);
+    //System.out.println(linkText);
+    //System.out.println(linkOuterH);
+    //System.out.println(linkInnerH);
+    
     SpringApplication.run(PayrollApplication.class, args);
   }
 }
