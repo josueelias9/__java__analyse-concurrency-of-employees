208b64e81c27a6827472aa46d1dc326ce7802133
Josué <josueeliashuaman@gmail.com>
2022-03-27 18:39:22 -0500

    simple front end

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,5 @@
 
-
-
-async function getAPI(url){
+async function getAPI(url) {
     let response = await fetch(url);
     let data_json = await response.json;
 
@@ -10,4 +8,24 @@ async function getAPI(url){
 
 
 
-document.getElementById("prueba").innerHTML = "ooeoeoe";
+let direccion_url = "http://localhost:8080/calcular";
+let a = await getAPI(direccion_url);
+
+document.getElementById("prueba").innerHTML = a;
+
+
+// 
+// 
+// const myForm = document.getElementById("myForm");
+// 
+// myForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const formData = new FormData(this);
+//     putapi(direccion_url, formData).then(() => {
+//         map.data.forEach(function (feature) {
+//             map.data.remove(feature);
+//         });
+//         map.data.loadGeoJson(direccion_url);
+//     });
+// 
+// });
