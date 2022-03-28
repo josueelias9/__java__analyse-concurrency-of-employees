4621631a0898eb60cde53b02822384b401fd23c3
Josué <josueeliashuaman@gmail.com>
2022-03-28 04:51:55 -0500

    Bootstrap added

--- frontend/main.js
+++ frontend/main.js
@@ -13,6 +13,7 @@ function mes(numero_de_mes) {
     else if (numero_de_mes == 10) return "octubre";
     else if (numero_de_mes == 11) return "noviembre";
     else if (numero_de_mes == 12) return "diciembre";
+    else if (numero_de_mes == 13) return "enero (2016)";
 }
 
 async function getAPI(url) {
@@ -52,14 +53,23 @@ async function postAPI(miurl, formData) {
             let a = "";
             console.log(data);
             for (let i = 0; i < data.length; i++) {
-                a = a + "<li>Del ";
-                a = a + data[i].dia + " de " + mes(data[i].mes) + "     a las " + data[i].hora + " horas";
-                a = a + " hasta el ";
-                a = a + data[i + 1].dia + " de " + mes(data[i].mes) + "     a las " + data[i + 1].hora + " horas";
-                a = a + "</li>";
+                a = a + '<tr>';
+                a = a + '<td>';
+                a = a + ((i/2)+1);
+                a = a + '</td>';
+                a = a + '<td>';
+                a = a + data[i].dia + " de " + mes(data[i].mes) + " a las " + data[i].hora + " horas";
+                a = a + '</td>';
+                a = a + '<td>';
+                a = a + data[i+1].dia + " de " + mes(data[i+1].mes) + " a las " + data[i+1].hora + " horas";
+                a = a + '</td>';
+
+                a = a + '</tr>';
                 i = i + 1;
             }
-            document.getElementById("prueba").innerHTML = a;
+            document.getElementById("oe").innerHTML = a;
+            //document.getElementById("oe").innerHTML = '<th scope="row">3</th> <td colspan="2">Larry the Bird</td>    <td>@twitter</td>';
+
         });
 
 }
